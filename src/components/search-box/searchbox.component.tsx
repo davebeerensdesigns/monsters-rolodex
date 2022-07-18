59df90f02f080dea72f55d54ac0eae1a506c0622
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-18 12:50:35 +0200

    Typescript Types & Third Party Library Types

--- src/components/search-box/searchbox.component.tsx
+++ src/components/search-box/searchbox.component.tsx
@@ -1,8 +1,14 @@
+import {ChangeEventHandler} from "react";
+
 import './search-box.style.css';
 
-const name: string = 345154;
+type SearchBoxProps = {
+    className: string,
+    placeholder?: string,
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+}
 
-const SearchBox = ({className, placeholder, onChangeHandler}) =>  (
+const SearchBox = ({className, placeholder, onChangeHandler} : SearchBoxProps) =>  (
             <input
                 className={`search-box ${className}`}
                 type='search'
